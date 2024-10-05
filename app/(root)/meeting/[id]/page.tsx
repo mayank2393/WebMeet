29c34a7987c49ced9674ee6d859ac5006a2419a6
Mayank Tripathi <mayankchamps27@gmail.com>
2024-10-05 14:57:34 +0530

    video streaming/meeting app using stream and clerk authentication

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,18 +1,39 @@
 "use client";
-import Loader from '@/components/Loader';
-import MeetingRoom from '@/components/MeetingRoom';
-import MeetingSetup from '@/components/MeetingSetup';
-import { UseGetCallById } from '@/hooks/UseGetCallById';
-import { useUser } from '@clerk/nextjs'
-import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
-import React, { useState } from 'react'
 
-const Meeting = ({params} : {params : {id : string}}) => {
- const {user,isLoaded} = useUser(); 
- const [isSetupComplete, setIsSetupComplete] = useState(false);
-  const {call,isCallLoading} = UseGetCallById(params.id);
+import { useState } from "react";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import { useParams } from "next/navigation";
+import { Loader } from "lucide-react";
 
-  if(!isLoaded || isCallLoading) return <Loader/>
+import { UseGetCallById } from "@/hooks/UseGetCallById";
+
+import MeetingSetup from "@/components/MeetingSetup";
+import MeetingRoom from "@/components/MeetingRoom";
+import Alert from "@/components/Alert";
+
+const MeetingPage = () => {
+  const { id } = useParams();
+  const { isLoaded, user } = useUser();
+  const { call, isCallLoading } = UseGetCallById(id);
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+  if (!isLoaded || isCallLoading) return <Loader />;
+
+  if (!call)
+    return (
+      <p className="text-center text-3xl font-bold text-white">
+        Call Not Found
+      </p>
+    );
+
+  // get more info about custom call type:  https://getstream.io/video/docs/react/guides/configuring-call-types/
+  const notAllowed =
+    call.type === "invited" &&
+    (!user || !call.state.members.find((m) => m.user.id === user.id));
+
+  if (notAllowed)
+    return <Alert title="You are not allowed to join this meeting" />;
 
   return (
     <main className="h-screen w-full">
@@ -27,6 +48,6 @@ const Meeting = ({params} : {params : {id : string}}) => {
       </StreamCall>
     </main>
   );
-}
+};
 
-export default Meeting;
+export default MeetingPage;
